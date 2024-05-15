9778ceafae91b44121384cdb5956f9339b6b6885
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-05-15 12:04:33 +0100

    working

---
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await dbContext.Category.ToListAsync() );
+        }
+    }
+}
